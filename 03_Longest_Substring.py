a94146faec70db2272dfb0d2b4353d313cfa21ce
ayushnegi099 <ayushnegi551@gmail.com>
2026-06-30 21:12:32 +0530

    Create 03_Longest_Substring.py
    Add LeetCode 3 solution

---
+++ 03_Longest_Substring.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        left = 0
+        seen = set()
+        maxLength = 0
+
+        for right in range(len(s)):
+
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+            maxLength = max(maxLength, right - left + 1)
+
+        return maxLength
+
+        
